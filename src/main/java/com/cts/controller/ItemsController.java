5ab8d8e2900c5df18e4a5d2876b4374429424206
PRASHANT SAHU <115061659+prashantsahu20@users.noreply.github.com>
2025-03-24 12:02:40 +0530

    Update ItemsController.java

--- src/main/java/com/cts/controller/ItemsController.java
+++ src/main/java/com/cts/controller/ItemsController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/items")
-@CrossOrigin(origins = {"http://localhost:3000", "https://yours-cafe-dine.netlify.app"})
+@CrossOrigin(origins = "https://yours-cafe-dine.netlify.app")
 public class ItemsController {
 
     @Autowired
