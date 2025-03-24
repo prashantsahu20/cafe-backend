5a25fdf4f3793df9e8a3830c6b5e7c8bd7f3f61e
PRASHANT SAHU <115061659+prashantsahu20@users.noreply.github.com>
2025-03-24 11:49:11 +0530

    Update SecurityConfig.java

--- src/main/java/com/cts/config/SecurityConfig.java
+++ src/main/java/com/cts/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.cts.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
 
 @Configuration
 @EnableWebSecurity
